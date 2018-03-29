af4be1da3e03d5c0414a338fdc9a880ee385aed5
Petar Koretic <petar.koretic@qaap.io>
2018-03-29 20:01:05 +0200

    use template string literals

--- examples/node/simple.js
+++ examples/node/simple.js
@@ -1,22 +1,24 @@
 const Canvas = require("canvas")
 const PDF417 = require("pdf417-generator")
 
-const code = "HRVHUB30\nHRK\n" +
-    "000000000012355\n"+
-    "PETAR KORETIĆ\n"+
-    "PREVOJ DD\n"+
-    "10000 Zagreb\n"+
-    "QAAP J.D.O.O\n"+
-    "PREVOJ DD\n"+
-    "10000 ZAGREB\n"+
-    "HR5041240000000000\n"+
-    "HR01\n"+
-    "7336-68949637625-00001\n"+
-    "COST\n"+
-    "Uplata za 1. mjesec\n";
+const code =
+`HRVHUB30
+HRK
+000000000012355
+PETAR KORETIĆ
+PREVOJ DD
+10000 Zagreb
+QAAP J.D.O.O
+PREVOJ DD
+10000 ZAGREB
+HR5041240000000000
+HR01
+7336-68949637625-00001
+COST
+Uplata za 1. mjesec`
 
 let canvas = new Canvas()
 PDF417.draw(code, canvas)
 
 // create image which can be sent in an e-mail or similar
-console.log('<img src="' + canvas.toDataURL() + '" />')
+console.log(`<img src="${canvas.toDataURL()}" />`)
