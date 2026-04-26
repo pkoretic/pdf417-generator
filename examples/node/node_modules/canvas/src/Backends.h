e73ac1e4c12f38634a40bdcda5ac25990abde6ca
Petar Koretic <petar.koretic@gmail.com>
2026-04-26 13:36:36 +0200

    update

--- examples/node/node_modules/canvas/src/Backends.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "backend/Backend.h"
-#include <napi.h>
-
-class Backends : public Napi::ObjectWrap<Backends> {
-  public:
-    static void Initialize(Napi::Env env, Napi::Object exports);
-};
